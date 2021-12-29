e7fff6f9c2526661af708f7c2d0e550cfc3a3d05
AlanFromJapan <alanfromjapan@gmail.com>
2021-12-29 20:32:12 +0900

    Now scans whole folder or file

--- mtganalyzer/main.py
+++ mtganalyzer/main.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from sqlite3.dbapi2 import paramstyle
 from flask import Flask, render_template, redirect, url_for, request, make_response
 import os, sys
 import logging
@@ -27,13 +28,27 @@ def homepage():
     return 'Hello world <img src="%s" />' % (mtgalib.getImageURLFromArenaID(78909, "small"))
 
 ########################################################################################
+## Non-web related functions
+#
+
+#---------------------------------------------------------------------------------------
+# Processes 1 file (path to the file)
+def scanOneFile (path):
+    print ("Scanning " + path)
+    mlist = scanner.scanFile(path)
+    for m in mlist:
+        db.storeMatch(m)
+
+########################################################################################
 ## Main entry point
 #
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        print ("Need to pass the path to a MTGA log file")
+        print ("Need to pass the path to a MTGA log file or folder of the logs")
         exit()
 
+
+    #Init
     myPlayerId = config.myconfig["my_userId"]
     print ("My user id " + myPlayerId)
 
@@ -45,13 +60,19 @@ if __name__ == '__main__':
         #scanner init
         scanner = mtgalogs.MtgaLogScanner(myPlayerId)
 
-        #scan (SAMPLE)
-        mlist = scanner.scanFile(str(sys.argv[1]))
-        for m in mlist:
-            db.storeMatch(m)
+        #scan 
+        paramPath = str(sys.argv[1])
+        if ".log" == paramPath[-4:].lower():
+            #scan 1 file
+            scanOneFile(paramPath)
+        else:
+            #assume it's a folder
+            for f in os.listdir(paramPath):
+                scanOneFile(os.path.join(paramPath, f))
 
         #start web interface
         app.debug = True
         app.run(host='0.0.0.0', port=45678, threaded=True)
+
     finally:
         pass
