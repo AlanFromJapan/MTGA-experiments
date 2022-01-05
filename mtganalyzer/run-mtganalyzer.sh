c1546cdc53995a3840dc818ed59df7df879782eb
Alan <alanfromjapan@gmail.com>
2022-01-05 20:33:06 +0900

    Added startup script, and started design of the deck stats page

--- mtganalyzer/run-mtganalyzer.sh
+++ mtganalyzer/run-mtganalyzer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd ~/Git/MTGA-experiments/mtganalyzer
+
 #will start in parallel, but normally by the time the website is opened the service has been launched
 xdg-open http://localhost:45678/
 
